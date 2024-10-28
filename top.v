89e82ce21cdb7c0560fb6984c456e43b5dcef6ad
Igor Michalak <git@igormichalak.com>
2024-10-28 08:13:37 +0100

    clock 9600hz

--- top.v
+++ top.v
@@ -4,20 +4,35 @@ module uart_peripheral(
     output clk_out
 );
 
-    wire clk;
+    wire clk_12mhz;
+    wire clk_9600hz;
 
     SB_HFOSC #(
-        .CLKHF_DIV("0b11")
+        .CLKHF_DIV("0b10")
     ) hfosc_inst (
         .CLKHFEN(1'b1),
         .CLKHFPU(1'b1),
-        .CLKHF(clk)
+        .CLKHF(clk_12mhz)
     );
 
     SB_GB clk_glob_buf (
-        .USER_SIGNAL_TO_GLOBAL_BUFFER (clk),
+        .USER_SIGNAL_TO_GLOBAL_BUFFER (clk_9600hz),
         .GLOBAL_BUFFER_OUTPUT (clk_out)
     );
+
+    reg [9:0] clk_counter;
+    reg clk_state;
+
+    always @(posedge clk_12mhz) begin
+        if (clk_counter >= 10'd624) begin
+            clk_counter <= 10'd0;
+            clk_state <= ~clk_state;
+        end else begin
+            clk_counter <= clk_counter + 1;
+        end
+    end
+
+    assign clk_9600hz = clk_state;
 
 endmodule
 
