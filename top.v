c2ab8fdf3c7ad0b2c9ec9a63649e23975da29c39
Igor Michalak <git@igormichalak.com>
2024-10-28 10:09:04 +0100

    UART state machine

--- top.v
+++ top.v
@@ -1,7 +1,8 @@
 module uart_peripheral(
-    input  uart_rx,
-    output uart_tx,
-    output clk_out
+    input      reset,
+    input      uart_rx,
+    output reg uart_tx,
+    output     clk_out
 );
 
     wire clk_12mhz;
@@ -33,6 +34,75 @@ module uart_peripheral(
     end
 
     assign clk_9600hz = clk_state;
+
+    localparam tx_idle  = 2'b00;
+    localparam tx_start = 2'b01;
+    localparam tx_data  = 2'b10;
+    localparam tx_stop  = 2'b11;
+
+    reg [1:0] tx_state;
+    reg [2:0] bit_counter;
+
+    reg [7:0] tx_data_reg;
+    reg       tx_sig_transmit;
+    reg       tx_sig_done;
+
+    always @(negedge reset) begin
+        tx_data_reg <= 8'h77; // ASCII 'w' (0x77)
+        tx_sig_transmit <= 1'b1;
+        tx_sig_done <= 1'b0;
+    end
+
+    always @(posedge clk_9600hz or negedge reset) begin
+        if (!reset) begin
+            tx_state <= tx_idle;
+            bit_counter <= 3'd0;
+        end else begin
+            case (tx_state)
+                tx_idle : begin
+                    if (tx_sig_transmit && !tx_sig_done) begin
+                        tx_state <= tx_start;
+                        bit_counter <= 3'd0;
+                    end
+                end
+                tx_start : begin
+                    tx_state <= tx_data;
+                end
+                tx_data : begin
+                    if (bit_counter == 3'd7) begin
+                        tx_state <= tx_stop;
+                    end else begin
+                        bit_counter <= bit_counter + 1;
+                    end
+                end
+                tx_stop : begin
+                    tx_state <= tx_idle;
+                    tx_sig_done <= 1'b1;
+                end
+            endcase
+        end
+    end
+
+    always @(posedge clk_9600hz or negedge reset) begin
+        if (!reset) begin
+            uart_tx <= 1'b1;
+        end else begin
+            case (tx_state)
+                tx_idle : begin
+                    uart_tx <= 1'b1;
+                end
+                tx_start : begin
+                    uart_tx <= 1'b0;
+                end
+                tx_data : begin
+                    uart_tx <= tx_data_reg[bit_counter];
+                end
+                tx_stop : begin
+                    uart_tx <= 1'b1;
+                end
+            endcase
+        end
+    end
 
 endmodule
 
